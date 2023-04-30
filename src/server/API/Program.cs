3165e7f66c9c873b87bc73d41a29e1f2cf934075
Articunatu <62301779+Articunatu@users.noreply.github.com>
2023-04-30 15:33:27 +0200

    fix: Automapping reference

--- src/server/API/Program.cs
+++ src/server/API/Program.cs
@@ -1,6 +1,7 @@
 using Core.Service;
 using Microsoft.Azure.Cosmos;
 using Models.Models;
+using AutoMapper;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -10,6 +11,7 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
 
 builder.Services.AddScoped<IRepository<Account>>(options =>
 {
--- src/server/API/ViewModels/ProfilePageModel.cs
+++ src/server/API/ViewModels/ProfilePageModel.cs
@@ -4,8 +4,8 @@ namespace API.ViewModels
     public record ProfilePageModel
     {
         public ProfileModel Profile;
-        public IEnumerable<PostModel> Posts;
-        public ProfilePageModel(ProfileModel profile, IEnumerable<PostModel> posts)
+        public IEnumerable<PostModel>? Posts;
+        public ProfilePageModel(ProfileModel profile, IEnumerable<PostModel>? posts)
         {
             Profile = profile;
             Posts = posts;
