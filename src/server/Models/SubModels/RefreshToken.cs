8836dd9b2d32b8ee906ee3c3b97f660b8e942e3c
Articunatu <62301779+Articunatu@users.noreply.github.com>
2023-06-11 09:59:37 +0200

    feat: Authentication models

---
+++ src/server/Models/SubModels/RefreshToken.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Models.SubModels
+{
+    public class RefreshToken
+    {
+        public string Text { get; set; } = string.Empty;
+        public DateTime Created { get; set; } = DateTime.Now;
+        public DateTime Expires { get; set; }
+    }
+}
