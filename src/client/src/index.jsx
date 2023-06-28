4f6609394ae475ff85b3d3bf0ea8aecd035e4354
Articunatu <62301779+Articunatu@users.noreply.github.com>
2023-06-28 16:40:06 +0200

    fix: Refactor routes so nav menu works

--- src/client/src/index.jsx
+++ src/client/src/index.jsx
@@ -2,17 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
-import { Routes, Route, BrowserRouter } from "react-router-dom";
-import RouteManager from './RouteManager';
+import RouterManager from './RouterManager';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
-      <Routes>
-        <Route path="/*" element={<RouteManager />} /> 
-      </Routes>
-    </BrowserRouter>
+    <RouterManager></RouterManager>
   </React.StrictMode>
 );
 
