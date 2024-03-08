514115dfe3dd72c3fa46c605c59a16c3bf93762d
Articunatu <62301779+Articunatu@users.noreply.github.com>
2024-03-08 13:56:35 +0100

    feat: Photo class

---
+++ code/server/SocialMedia/SocialMedia.Domain/Photos/Photo.cs
@@ -0,0 +1,12 @@
+﻿
+namespace SocialMedia.Domain.Photos
+{
+    internal class Photo
+    {
+        public Guid Id { get; set; }
+        public byte[] Base64 { get; set; }
+        //size, check if vertical or horizontal
+        public bool IsProfilePhoto { get; set; }
+        public bool IsBackgroundPhoto { get; set; }
+    }
+}
