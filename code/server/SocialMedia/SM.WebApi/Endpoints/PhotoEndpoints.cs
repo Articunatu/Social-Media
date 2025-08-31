49f7166c9648f244d86c219d3689871996828fbb
Articunatu <62301779+Articunatu@users.noreply.github.com>
2025-08-31 20:14:02 +0200

    feat: Photo endpoints

---
+++ code/server/SocialMedia/SM.WebApi/Endpoints/PhotoEndpoints.cs
@@ -0,0 +1,15 @@
+﻿namespace SM.WebApi.Endpoints;
+
+public static class PhotoEndpoints
+{
+    public static RouteGroupBuilder MapPhotoEndpoints(this IEndpointRouteBuilder routes)
+    {
+        var group = routes.MapGroup("/api/comments/");
+
+        group.MapPost("upload", UploadPhoto);
+        group.MapGet("get-by-post-id/{postId}", GetComments);
+        group.MapGet("{id}", GetCommentById);
+
+        return group;
+    }
+}
