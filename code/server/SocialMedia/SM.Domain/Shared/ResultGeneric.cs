8ff6b2993c9af0929bdf4138732c7ec204795c9c
Articunatu <62301779+Articunatu@users.noreply.github.com>
2025-11-04 15:50:06 +0100

    fix: Styling fixes and generic result errors

--- code/server/SocialMedia/SM.Domain/Shared/ResultGeneric.cs
+++ code/server/SocialMedia/SM.Domain/Shared/ResultGeneric.cs
@@ -6,33 +6,28 @@ public class Result<T> : Result
 {
     private readonly T? _value;
 
-    protected internal Result(T value, bool isSuccess, Error error, HttpStatusCode status)
+    protected internal Result(T? value, bool isSuccess, Error error, HttpStatusCode status)
         : base(isSuccess, error, status)
     {
-        if (isSuccess && value == null)
-            throw new ArgumentNullException(nameof(value), "Successful result must have a non-null value.");
-
         _value = value;
     }
 
     protected internal Result(bool isSuccess, Error error, HttpStatusCode status)
         : base(isSuccess, error, status)
     {
-        if (isSuccess)
-            throw new InvalidOperationException("Successful result must have a value.");
-
         _value = default;
     }
 
-    public T Value => IsSuccess ? _value! : throw new InvalidOperationException("No value present.");
+    public T? Value => _value;
 
-    public static implicit operator Result<T>(T value) => Create(value);
-
-    public static Result<T> Create(T value)
+    public bool TryGetValue(out T? value)
     {
-        if (value == null)
-            throw new ArgumentNullException(nameof(value), "The value cannot be null.");
-
-        return new Result<T>(value, true, Error.None, HttpStatusCode.OK);
+        value = _value;
+        return IsSuccess;
     }
+
+    public static implicit operator Result<T>(T? value) => Create(value);
+
+    public static Result<T> Create(T? value)
+        => new(value, true, Error.None, HttpStatusCode.OK);
 }
